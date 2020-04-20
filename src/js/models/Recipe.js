8475482e96619d4f339c0be703b6d91e04c1f67c
marts-dev <8363978+marts-dev@users.noreply.github.com>
2020-04-20 22:04:27 +0800

    Recipe model

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -10,10 +10,25 @@ export default class Recipe {
       const res = await axios(
         `https://forkify-api.herokuapp.com/api/get?rId=${this.id}`
       );
+      this.title = res.data.recipe.title;
+      this.author = res.data.recipe.author;
+      this.url = res.data.recipe.source_url;
+      this.ingredients = res.data.recipe.ingredients;
+      this.image_url = res.data.recipe.image_url;
       console.log(res.data);
       this.result = res.data.recipe;
     } catch (error) {
       console.log(error);
     }
+  }
+
+  calcTime() {
+    const numIng = this.ingredients.length;
+    const periods = Math.ceil(numIng / 3);
+    this.time = periods * 15;
+  }
+
+  calcServings() {
+    this.servings = 4;
   }
 }
